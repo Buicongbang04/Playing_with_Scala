cc80bef2ff23ba3328f0e0d64a6332789fccd4e5
Buicongbang04 <buicongbang1010@gmail.com>
2025-02-18 22:58:55 +0700

    add something new

--- 1. Get Started/main.scala
+++ 1. Get Started/main.scala
@@ -1,5 +1,30 @@
-object HelloWorld{
+import scala.io.StdIn.readLine
+
+object Main{
     def main(args: Array[String]): Unit = {
-        println("Hello, world!")
+        println("Hello world!")
+        println(msg)
+        // input_temperature
+        var doanh = People("Doanh", 22, 170.5)
+        doanh.printInfo()
+    }
+}
+def msg = "I was compiled with Scala :)"
+
+// Asking for user input
+def input_temperature = {
+    println("Please enter the temperature in Celsius: ")
+    val celsius = readLine().toDouble //import scala.io.StdIn.readLine
+    val fahrenheit = celsius * 9/5 + 32
+    println(s"Temperature in Fahrenheit: ${fahrenheit} degrees")
+    println(s"Temperature in Celsius: ${celsius} degrees")
+}
+
+// Define a class
+class People(name: String, age: Int, height: Double){
+    def printInfo(): Unit = {
+        println(s"Name: ${name}")
+        println(s"Age: ${age}")
+        println(s"Height: ${height} cm")
     }
 }
