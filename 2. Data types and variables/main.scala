111128cc6e4b2d07a70bd39679ac90cb0414277d
Buicongbang04 <buicongbang1010@gmail.com>
2025-02-19 19:31:06 +0700

    add new code

---
+++ 2. Data types and variables/main.scala
@@ -0,0 +1,31 @@
+
+
+object Demo {
+   // def main(args: Array[String]) : Unit = {
+   //    var myVar :Int = 10
+   //    val myVal :String = "Hello Scala with datatype declaration."
+   //    var myVar1 = 20
+   //    val myVal1 = "Hello Scala new without datatype declaration."
+         
+   //    println(myVar)
+   //    println(myVal)
+   //    println(myVar1)
+   //    println(myVal1)
+   // }
+
+   def main2(args: Array[String]) : Unit = {
+      val obj = PropertyExample()
+      obj.display()
+      println(obj.quantity)
+      // println(obj.price) // This line will give an error because the price is private.
+   }
+}
+
+class PropertyExample {
+   var quantity = 15
+   private var price = 50
+   def display() : Unit = {
+      println("Quantity: " + quantity)
+   }
+   println("Price: " + price)
+}
