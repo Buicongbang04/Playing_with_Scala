8f6c991d21e1467f0e66c889e3e8fd5ec39454be
Buicongbang04 <buicongbang1010@gmail.com>
2025-02-19 22:47:03 +0700

    add new code

--- 2. Data types and variables/main.scala
+++ 2. Data types and variables/main.scala
@@ -12,11 +12,36 @@ object Day_2 {
       // println(myVar1)
       // println(myVal1)
 
-
+      // Scope in Scala
       val obj = PropertyExample()
       obj.display()
       println(obj.quantity)
       // println(obj.price) // This line will give an error because the price is private.
+
+      //Array and for loop
+      var myList = Array(1.9, 2.9, 3.4, 3.5)
+      
+      // Print all the array elements
+      for ( x <- myList ) {
+         println( x )
+      }
+
+      // Summing all elements
+      var total = 0.0;
+      
+      for ( i <- 0 to (myList.length - 1)) {
+         total += myList(i);
+      }
+      println("Total is " + total);
+
+      // Finding the largest element
+      var max = myList(0);
+      
+      for ( i <- 1 to (myList.length - 1) ) {
+         if (myList(i) > max) max = myList(i);
+      }
+      
+      println("Max is " + max);
    }
 }
 
